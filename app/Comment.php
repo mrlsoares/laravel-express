7fb1b52074ae30d087b6cf73227c88de0e5a7a37
Marcos Soares <mrlsoares@gmail.com>
2015-07-15 17:16:42 -0300

    Adicionado Models Post | Comment ;

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $fillable=['post_id','comment','name','email'];
+
+    public  function post()
+    {
+        return $this->belongsTo('App\Post');
+    }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,6 +10,13 @@
 
         <h2>{{$post->title}}</h2>
         <p>{{$post->content}}</p>
+        <h3>Comment</h3>
+        @foreach($post->comments as $coment)
+            <b>Name</b>:{{$coment->name}}<br>
+            <b>Comment</b>:{{$coment->comment}}
+
+        @endforeach
+
 <hr/>
     @endforeach
 
